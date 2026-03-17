aefaf67fd5d1d23896c861a6a1e5513eed349c81
aditi <aditi@LAPTOP-NQ8BEARG>
2026-03-17 06:12:23 +0530

    Ques

--- src/Lec13_Function_Ques/Print_Armstrong_Numbers.java
+++ src/Lec13_Function_Ques/Print_Armstrong_Numbers.java
@@ -8,16 +8,16 @@ public class Print_Armstrong_Numbers {
 		// TODO Auto-generated method stub
 
 		Scanner sc= new Scanner(System.in);
-		int N1=sc.nextInt();
-		int N2=sc.nextInt();
+		int N1=sc.nextInt(); //starting of number
+		int N2=sc.nextInt(); // ending of number
 		
 		int count=N1;
-		while(count<=N2)
+		while(count<=N2) //Loop to print armstrong numbers between N1 to N2
 		{
-		int ans=isarmstrong(count);
-		if(ans !=0)
-		{
-		System.out.println(ans);
+		int ans=isarmstrong(count); 
+		if(ans !=0) //I do this bcoz only 407 is armstrong number between 400 to 1000 
+		{           // And for every another number we get 0 as return so we cant print all the 0's
+		System.out.println(ans); //So if the ans is not equal to 0 then it print that number which is armstrong
 		}
 		count++;
 		}
@@ -38,11 +38,11 @@ public class Print_Armstrong_Numbers {
 		N=N/10;
 		}
 
-		if(number==sum)
+		if(number==sum) //If we find the armstrong number then return that number
 		{
 		return number;
 		}
-		else
+		else  //It is must to write this if the number between N1 tO N2 is not a armstrong number we return 0
 		{
 		return 0;
 		}
